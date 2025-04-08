474e71a10c17dcab489a436c3e8616226c552adb
mslavik <mironovsvatoslav38@gmail.com>
2025-04-08 16:28:49 +0300

    Add Mironov_MathTask.h

---
+++ Mironov_MathTask.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Mironov_MathTask.h"
+
+using namespace std;
+
+int main()
+{
+    int NumberA = 0, NumberB = 0;
+    //ввод переменной А
+    EnterDigit(NumberA, "input height A");
+    //ввод переменной В
+    EnterDigit(NumberB, "input height B");
+
+    //вычисление площади прямоугольника
+    int RectangleArea = CalcRectangleArea(NumberA, NumberB);
+    //вывод значения площади
+    cout << "Area of Rectangle is " << RectangleArea << endl;
+}
